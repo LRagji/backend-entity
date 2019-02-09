f818677db95ac813eb02dd96f32fc98823419deb
Laukik On Windows Home <22937426+LRagji@users.noreply.github.com>
2019-02-09 19:37:44 +0530

    firstCommit:Getting things in line

---
+++ filterbuilder.js
@@ -0,0 +1,99 @@
+class filterBuilder {
+
+    constructor(operatorMap) {
+
+        this.addOperatorConditionFor = this.addOperatorConditionFor.bind(this);
+        this.removeOperatorConditionFor = this.removeOperatorConditionFor.bind(this);
+        this.sortByConditionFor = this.sortByConditionFor.bind(this);
+        this.removeSortByConditionFor = this.removeSortByConditionFor.bind(this);
+
+        this._addConditions = this._addConditions.bind(this);
+        this._removeConditions = this._removeConditions.bind(this);
+
+
+        this._singleOperandsOperators = Object.entries(operatorMap).filter(kvp => kvp[1] === "singleOperand").map(kvp => kvp[0]);
+        this._multiOperandsOperators = Object.entries(operatorMap).filter(kvp => kvp[1] === "multiOperand").map(kvp => kvp[0]);
+        this._ascendingOperator = Object.entries(operatorMap).filter(kvp => kvp[1] === "asc").map(kvp => kvp[0])[0];
+        this._descendingOperator = Object.entries(operatorMap).filter(kvp => kvp[1] === "desc").map(kvp => kvp[0])[0];
+    }
+
+    addOperatorConditionFor(filterObject, operator, propertyName, propertyValue) {
+
+        let isSingle = this._singleOperandsOperators.indexOf(operator) >= 0;
+        let isMulti = this._multiOperandsOperators.indexOf(operator) >= 0;
+        if (isMulti === false && isSingle === false) throw new Error(operator + " is not a operator, or is not defined.")
+        if (isMulti & isSingle === true) throw new Error(operator + " is not Invalid operator, or operator defination is incorrect.")
+
+        return this._addConditions(filterObject, operator, propertyName, propertyValue, isSingle, isMulti);
+    }
+
+    removeOperatorConditionFor(filterObject, operator, propertyName, propertyValue) {
+
+        let isSingle = this._singleOperandsOperators.indexOf(operator) >= 0;
+        let isMulti = this._multiOperandsOperators.indexOf(operator) >= 0;
+        if (isMulti === false && isSingle === false) throw new Error(operator + " is not a operator, or is not defined.")
+        if (isMulti & isSingle === true) throw new Error(operator + " is not Invalid operator, or operator defination is incorrect.")
+
+        return this._removeConditions(filterObject, operator, propertyName, propertyValue, isSingle, isMulti);
+    }
+
+    sortByConditionFor(filterObject, propertyName, ascending, priority) {
+
+        return this._addConditions(filterObject, ascending === true ? this._ascendingOperator : this._descendingOperator, propertyName, priority, true, false);
+    }
+
+    removeSortByConditionFor(filterObject, propertyName, ascending, priority) {
+
+        return this._removeConditions(filterObject, ascending === true ? this._ascendingOperator : this._descendingOperator, propertyName, priority, true, false);
+    }
+
+    _addConditions(filterObject, operator, propertyName, propertyValue, isSingle, isMulti) {
+        if (filterObject[operator] === undefined) {
+            //Operator is not defined
+            filterObject[operator] = {};
+            if (isSingle === true) filterObject[operator][propertyName] = propertyValue;
+            if (isMulti === true) filterObject[operator][propertyName] = [propertyValue];
+        }
+        else {
+            //Operator is defined but this property is not defined
+            //OR
+            //Operator is defined property is also defined still overwrite the value to what we have.
+            if (isSingle === true) filterObject[operator][propertyName] = propertyValue;
+            if (isMulti === true) {
+                if (filterObject[operator][propertyName] === undefined)
+                    filterObject[operator][propertyName] = [propertyValue];
+                else
+                    filterObject[operator][propertyName].push(propertyValue);
+            }
+        }
+        return filterObject;
+    }
+
+    _removeConditions(filterObject, operator, propertyName, propertyValue, isSingle, isMulti) {
+        if (filterObject[operator] === undefined) {
+            //Operator is not defined
+        }
+        else {
+            if (filterObject[operator][propertyName] === undefined) {
+                //Operator is defined but this property is not defined
+            }
+            else {
+                //Operator is defined property is also defined still overwrite the value to what we have.
+                if (isSingle === true) delete filterObject[operator][propertyName];
+                if (isMulti === true) {
+                    let idx = filterObject[operator][propertyName].indexOf(propertyValue);
+                    if (idx >= 0) {
+                        filterObject[operator][propertyName].splice(idx, 1);
+                    }
+                    if (filterObject[operator][propertyName].length === 0) delete filterObject[operator][propertyName];
+                }
+            }
+            if (Object.keys(filterObject[operator]).length === 0)
+                delete filterObject[operator];
+        }
+        return filterObject;
+    }
+
+}
+
+module.exports = filterBuilder;
---
+++ filterqueryparserpg.js
@@ -0,0 +1,82 @@
+class filterQueryParserPg {
+
+    constructor(propertyMap) {
+        this._propertyMap = propertyMap;
+        this._operatorMap = {
+            "like": "like",
+            "equal": "=",
+            "greaterThan": ">",
+            "lessThan": "<",
+            "ascending": "asc",
+            "descending": "desc",
+            "containsArr": "&&"
+        };
+
+        this.constructWhereClause = this.constructWhereClause.bind(this);
+        this.constructOrderByClause=this.constructOrderByClause.bind(this);
+    }
+
+    constructWhereClause(filterObject, argumentArray) {
+        let whereClause = ""
+      
+        if (!Array.isArray(argumentArray)) throw new Error("Parameter 'argumentArray' cannot be undefined, has to be of array data type.");
+
+        Object.keys(filterObject).forEach((operator) => {
+
+            switch (operator) {
+                case 'equal':
+                case 'greaterThan':
+                case 'lessThan':
+                    Object.keys(filterObject[operator]).forEach((operand) => {
+                        whereClause += (whereClause === "" ? "" : " and ") + this._propertyMap[operand] + " " + this._operatorMap[operator] + " $" + (argumentArray.length + 1);
+                        argumentArray.push(filterObject[operator][operand]);
+                    });
+                    break;
+                case 'like':
+                    Object.keys(filterObject[operator]).forEach((operand) => {
+                        whereClause += (whereClause === "" ? "" : " and ") + " lower(" + this._[operand] + ") " + this._operatorMap[operator] + " $" + (argumentArray.length + 1);
+                        argumentArray.push("%" + filterObject[operator][operand].toLowerCase() + "%");
+                    });
+                    break;
+                case 'containsArr':
+                    Object.keys(filterObject[operator]).forEach((operand) => {
+                        whereClause += (whereClause === "" ? "" : " and ") + this._propertyMap[operand] + " " + this._operatorMap[operator] + " $" + (argumentArray.length + 1);
+                        argumentArray.push(filterObject[operator][operand]);
+                    });
+                    break;
+                default:
+                    console.warn("New Operator found: " + operator)
+                    break;
+            }
+        });
+
+        return whereClause;
+
+    }
+
+    constructOrderByClause(filterObject) {
+        let orderClause = ""
+       
+        Object.keys(filterObject).forEach((operator) => {
+
+            switch (operator) {
+                case 'ascending':
+                case 'descending':
+                    Object.keys(filterObject[operator]).forEach((operand) => {
+                        orderClause += (orderClause === "" ? "" : " , ") + this._propertyMap[operand] + " " + this._operatorMap[operator];
+                    });
+                    break;
+                default:
+                    console.warn("New Operator found: " + operator)
+                    break;
+            }
+        });
+
+        return orderClause;
+
+    }
+
+
+}
+
+module.exports = filterQueryParserPg;
